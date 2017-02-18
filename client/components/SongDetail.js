e3f18ab19063fb0910ed12180f686e84341d4469
jorissparla <joris.sparla@gmail.com>
2017-02-18 19:29:16 +0100

    initial

---
+++ client/components/SongDetail.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import fetchSong from '../queries/fetchSong'
+
+class SongDetail extends Component {
+
+  componentDidMount() {
+    const id = this.props.params.id;
+
+  }
+
+
+  render () {
+    if (this.props.data.loading) {
+      return <div>Loading</div>
+    }
+    const { id, title } = this.props.data.song
+    return (
+      <div>
+      <Link to='/'>Back</Link>
+        <h3>SongDetail</h3>
+        {title}
+      </div>
+    )
+  }
+}
+
+
+
+export default graphql(fetchSong, {
+  options: (props)=> { return { variables: { id: props.params.id} }}}
+)(SongDetail)
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://jsparla:jsparla@ds155529.mlab.com:55529/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
